9dae605faf6f5737215d65824f61a95d9dd4cc19
First Name <you@example.com>
2018-03-18 10:06:29 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> SignUp Login Page</title>
+  </head>
+ <body>
+  <button type ="button" onclick="window.location.href='signup.php'"> Sign Up</button>
+  <button type ="button" onclick="window.location.href='signin.php'"> Sign Up</button>
+ </body>
+</html> 
